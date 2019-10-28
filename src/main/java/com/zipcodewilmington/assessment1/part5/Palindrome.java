9b24b52ab83937bb178fabe01713d710cdbb00d4
Mike Gray <mike@zipcoders-MacBook-Pro.local>
2019-10-28 14:54:54 -0400

    Almost done

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -5,24 +5,11 @@ import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-//        int counter = input.length();
-//        for(int i = 0; i < input.length()/2; i++){
-//            for(int j = i + 2;j <= input.length();j++){
-//                String check = input.substring(i, j);
-//                String check2 = input.substring(input.length() - j, input.length() - i);
-//                if(check.equals(BasicStringUtils.reverse(check))){
-//                    counter++;
-//                }
-//                if(check2.equals(BasicStringUtils.reverse(check)) /*&& input.length() - j != i*/){
-//                    counter++;
-//                }
-//            }
-//        }
         int counter = input.length();
         for(int i = 0; i < input.length(); i++){
             for(int j = i + 2;j <= input.length();j++){
                 String check = input.substring(i, j);
-                if(check.equals(BasicStringUtils.reverse(check))){
+                if(isPalindromic(check)){
                     counter++;
                 }
             }
@@ -30,5 +17,13 @@ public class Palindrome {
         return counter;
     }
 
+    public static boolean isPalindromic(String input) {
 
+        for(int i = 0; i < input.length() / 2; i++){
+            if(input.charAt(i) != (input.charAt(input.length()- i - 1))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
