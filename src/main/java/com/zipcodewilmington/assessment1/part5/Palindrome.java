e737ac4585d6b94c58445e5904887e46359625c6
Mike Gray <mike@zipcoders-MacBook-Pro.local>
2019-10-28 15:03:40 -0400

    Finished

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -6,10 +6,14 @@ public class Palindrome {
 
     public Integer countPalindromes(String input){
         int counter = input.length();
-        for(int i = 0; i < input.length(); i++){
+        for(int i = 0; i < input.length() / 2; i++){
             for(int j = i + 2;j <= input.length();j++){
                 String check = input.substring(i, j);
+                String check2 = input.substring(input.length() - j, input.length() - i);
                 if(isPalindromic(check)){
+                    counter++;
+                }
+                if(isPalindromic(check2) && input.length()-j != i){
                     counter++;
                 }
             }
